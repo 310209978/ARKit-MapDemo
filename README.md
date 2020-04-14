c61b3d1af1b56ee41c3d0e017864b66f2e1969bf
elaine <310209978@qq.com>
2020-04-14 22:32:25 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# ARKit-MapDemo
+Persistence gives your app a way to save the current augmented reality view so the user can load it and view it again. This can be handy if an augmented reality session can take place over an extended period of time, such as an augmented reality game where users can save the game and return to it at another time.</br>
+
+Saving a world map in the user defaults is the simplest way to store an augmented reality view. The key is simply giving the user the ability to save a world map, load it at a later time, and clear the augmented reality view.
--- STry4/ViewController.swift
+++ STry4/ViewController.swift
@@ -10,25 +10,111 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    @IBOutlet var lblMessage: UILabel!
+    
+    let configuration = ARWorldTrackingConfiguration()
+    
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        guard let sceneView = sender.view as? ARSCNView else {
+    return
+    }
+    let touch = sender.location(in: sceneView)
+    let hitTestResults = sceneView.hitTest(touch, types: [.featurePoint, .estimatedHorizontalPlane])
+    if hitTestResults.isEmpty == false {
+    if let hitTestResult = hitTestResults.first {
+    let virtualAnchor = ARAnchor(transform: hitTestResult.worldTransform)
+        self.sceneView.session.add(anchor: virtualAnchor)
+    } }
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+    if anchor is ARPlaneAnchor { return
+    }
+    let newNode = SCNNode(geometry: SCNBox(width: 0.05, height: 0.05, length: 0.05, chamferRadius: 0))
+        newNode.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        node.addChildNode(newNode)
+        }
+
+    
+    func saveMap(){
+        self.sceneView.session.getCurrentWorldMap { worldMap, error in
+        if error != nil { print(error?.localizedDescription ?? "Unknown error")
+        return
+        }
+        if let map = worldMap {
+        let data = try! NSKeyedArchiver.archivedData(withRootObject: map, requiringSecureCoding: true)
+                        // save in user defaults database
+        let savedMap = UserDefaults.standard
+            savedMap.set(data, forKey: "worldmap")
+            savedMap.synchronize()
+            DispatchQueue.main.async {
+                self.lblMessage.text = "World map saved" }
+        }
+            
+        }
+        
+    }
+    
+    func loadMap() {
+    let storedData = UserDefaults.standard
+    if let data = storedData.data(forKey: "worldmap") {
+    if let unarchived = try? NSKeyedUnarchiver.unarchivedObject(ofClasses: [ARWorldMap.classForKeyedUnarchiver()], from: data), let worldMap = unarchived as? ARWorldMap {
+    let configuration = ARWorldTrackingConfiguration()
+        configuration.initialWorldMap = worldMap
+        configuration.planeDetection = .horizontal
+        self.lblMessage.text = "Previous world map loaded"
+        sceneView.session.run(configuration)
+    }
+    } else {
+    let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        sceneView.session.run(configuration)
+    } }
+    
+    
+    func clearMap() {
+    let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        self.lblMessage.text = "Tap to place a virtual object"
+        sceneView.debugOptions = [.showWorldOrigin, .showFeaturePoints]
+    let options: ARSession.RunOptions = [.resetTracking,.removeExistingAnchors]
+    sceneView.session.run(configuration, options: options)
+    }
+    
+    @IBAction func saveButton(_ sender: UIButton) {
+        saveMap()
+    }
+    
+    @IBAction func clearButton(_ sender: UIButton) {
+        clearMap()
+    }
+    @IBAction func loadButton(_ sender: UIButton) {
+        loadMap()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        
         // Set the view's delegate
         sceneView.delegate = self
+        sceneView.session.delegate = self
+        configuration.planeDetection = .horizontal
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+      let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        sceneView.addGestureRecognizer(tapGesture)
         
-        // Set the scene to the view
-        sceneView.scene = scene
-    }
+       self.lblMessage.text = "Tap to place a virtual object"
+       sceneView.session.run(configuration)
+       }
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
